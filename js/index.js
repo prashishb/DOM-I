1edb61cad46baecf78ad755944caf142e2306dfb
prashishb <prashb.web@gmail.com>
2021-09-27 14:32:24 -0400

    completed cta section

--- js/index.js
+++ js/index.js
@@ -39,8 +39,7 @@ const siteContent = {
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
-// navbar
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
 let navLinks = document.querySelectorAll('nav a');
 navLinks[0].textContent = siteContent['nav']['nav-item-1'];
 navLinks[1].textContent = siteContent['nav']['nav-item-2'];
@@ -48,6 +47,10 @@ navLinks[2].textContent = siteContent['nav']['nav-item-3'];
 navLinks[3].textContent = siteContent['nav']['nav-item-4'];
 navLinks[4].textContent = siteContent['nav']['nav-item-5'];
 navLinks[5].textContent = siteContent['nav']['nav-item-6'];
-// header image
-const headerImg = document.querySelector('#cta-img');
-headerImg.setAttribute('src', siteContent['cta']['img-src'])
+// cta
+const ctaImg = document.querySelector('#cta-img');
+ctaImg.setAttribute('src', siteContent['cta']['img-src']);
+const ctaHeadline = document.querySelector('h1');
+ctaHeadline.innerHTML = siteContent['cta']['h1'].split(' ').join('<br>');
+const btn = document.querySelector('button');
+btn.textContent = siteContent['cta']['button'];
